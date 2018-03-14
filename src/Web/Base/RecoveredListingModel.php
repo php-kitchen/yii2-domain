a6bd678c0199bdaa783ceefe06fdd3fc18152af4
Dmitry Bukavin <djaconda@bitfocus.com>
2018-03-14 21:50:55 +0200

    Added action for restore entity

---
+++ src/Web/Base/RecoveredListingModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PHPKitchen\Domain\Web\Base;
+
+use PHPKitchen\Domain\Data\EntitiesProvider;
+
+/**
+ * Represents a view model designed to be used in recovered listing actions.
+ *
+ * @package PHPKitchen\Domain\Web\Base
+ * @author Dmitry Bukavin <4o.djaconda@gmail.com>
+ */
+class RecoveredListingModel extends ListingModel {
+    /**
+     * Override this method
+     *
+     * @return \PHPKitchen\Domain\Data\EntitiesProvider
+     */
+    public function getDeletedDataProvider(): EntitiesProvider {
+        $provider = $this->getDataProvider();
+        $provider->query->deleted();
+
+        return $provider;
+    }
+}
